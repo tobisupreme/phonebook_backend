aa8c7218e4fbc7b95c1d96f323ae74467bd57bf8
Tobi Balogun <tobi.balo@gmail.com>
2022-09-05 09:23:42 +0100

    Implement "/api/persons/" route

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 const express = require('express')
 const app = express()
 
+let entries = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Yemi Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Esther Poppendieck',
+    number: '39-23-6423122',
+  },
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(entries).end()
+})
+
 const PORT = 3001
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is listening on ${PORT}`)
